e788e661e35515810933dc21c0c02c0b2b57023a
Vahe Charchyan <vcharchian@gmail.com>
2022-04-05 08:59:16 +0400

    app.py has been changed (app run part)

--- app.py
+++ app.py
@@ -138,5 +138,7 @@ def predict():
     except Exception as e:
         return render_template('index.html', prediction_text=str(e))
 
+port = int(os.getenv('PORT'))
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
